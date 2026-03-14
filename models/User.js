d38e50756fda2eca95293ffbf573bcf7c5ae6046
rk30207241300666@depi.eui.edu.eg <rk30207241300666@depi.eui.edu.eg>
2026-03-14 13:56:36 +0200

    add

--- models/User.js
+++ models/User.js
@@ -4,10 +4,10 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['employee', 'manager', 'admin'], default: 'employee' }, // [cite: 51]
-  leaveBalance: { type: Number, default: 21 }, // [cite: 52]
-  managerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // [cite: 53]
-  isActive: { type: Boolean, default: true } // [cite: 25] لتعطيل الحساب
+  role: { type: String, enum: ['employee', 'manager', 'admin'], default: 'employee' }, 
+  leaveBalance: { type: Number, default: 21 }, 
+  managerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
+  isActive: { type: Boolean, default: true } 
 });
 
 module.exports = mongoose.model("User", userSchema);
--- routes/leaveRoutes.js
+++ routes/leaveRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { submitLeave, reviewLeave, getMyLeaves } = require("../controllers/leaveControllers");
 const { verifyToken, checkRole } = require("../middleware/auth");
 
-router.post("/submit", verifyToken, submitLeave); // [cite: 11]
-router.get("/my-history", verifyToken, getMyLeaves); // [cite: 13]
-router.post("/review", verifyToken, checkRole(['manager', 'admin']), reviewLeave); // [cite: 19]
+router.post("/submit", verifyToken, submitLeave); 
+router.get("/my-history", verifyToken, getMyLeaves); 
+router.post("/review", verifyToken, checkRole(['manager', 'admin']), reviewLeave); 
 
 module.exports = router;
